49c2992e92b7696dda749f36495698862576af4b
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2021-03-25 23:24:32 -0300

    atualizando, agora esta recebendo mensagem e enviando para todos

--- Tarefa 1/TCP/tcpCliente.py
+++ Tarefa 1/TCP/tcpCliente.py
@@ -3,10 +3,12 @@ import threading
 
 
 def rodaThread(mySocket):
-    # recebe a devolução da mensagem do servidor
-    data = mySocket.recv(1024)
     
-    print('Recebido do servidor {}: {}'.format( mySocket.getpeername(),data.decode()))
+    while True:
+        # recebe a devolução da mensagem do servidor
+        data = mySocket.recv(1024)
+        
+        print('Recebido do servidor {}: {}'.format( mySocket.getpeername(),data.decode()))
 
 
 
@@ -21,22 +23,24 @@ def Main():
     #realiza a conexao com o servidor
     mySocket.connect((host,port))
     
+
+    
     t = threading.Thread(target=rodaThread, args=(mySocket,))
     t.start()
     
-    #aguarda o usuário digitar uma mensagem
-    message = input(" -> (q sair) ")
-    
-    while message != 'q' and message:
+    while True:
+        nome = input("Digite seu nome: ")
+        #aguarda o usuário digitar uma mensagem
+        message = input("Digite sua mensagem:")
+        nova = nome.encode()+message.encode()
         
         # envia a mensagem do usuário para o servidor
         mySocket.send(message.encode())
-              
-        #aguarda nova mensagem do usuário
-        message = input(" -> (q sair) ")
         
         
     mySocket.close()
     
+
+
 if __name__ == '__main__':
     Main()
--- Tarefa 1/TCP/tcpServidor.py
+++ Tarefa 1/TCP/tcpServidor.py
@@ -1,11 +1,21 @@
 import socket
 import threading
 
+lista = []
+
 def rodaThread(conn):
     while True:
+        
 
         print('Esperando mensagens...')
         data = conn.recv(1024)
+
+        nova = data.decode()
+        
+        for x in lista:
+            if conn != x:
+                x.send(nova.encode())
+        
         if not data:
             print('Fechando a conexão')
             conn.close()
@@ -14,7 +24,7 @@ def rodaThread(conn):
         #Se tiver dados
         print('Recebido {} bytes de {}'.format(len(data), conn.getpeername()))
         # devolve a mensagem para o cliente
-        conn.sendall(data.upper())
+        conn.sendall(data)
 
 
     conn.close()
@@ -35,9 +45,12 @@ def Main():
     print('Servidor TCP: {}:{}'.format(host,port))
     
     while True:
-        
+
         # fica bloqueado aguardando a conexão de um cliente
         conn, addr = socketTCP.accept()
+        
+        lista.append(conn)
+        
         print ("Conexão realizada por: " + str(addr))
         # cria e dispara a execução da thred do cliente
         t = threading.Thread(target=rodaThread, args=(conn,))
